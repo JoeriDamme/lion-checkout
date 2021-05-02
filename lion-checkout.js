10badd219e6104ad5c22624c1b1defe2746f5656
Joeri Damme <joeri.damme@kpn.com>
2021-05-02 14:46:10 +0200

    feat: Fetching basket data on load of WC.

--- lion-checkout.js
+++ lion-checkout.js
@@ -1,6 +1,46 @@
 import { LitElement, html } from 'lit-element';
+import { ajax } from '@lion/ajax';
 
 class LionCheckout extends LitElement {
+  /**
+   * Get properties.
+   */
+  static get properties() {
+    return {
+      basket: {
+        type: Object
+      },
+    }
+  }
+
+  constructor() {
+    super();
+    this.basket = {};
+  }
+
+  /**
+   * Get basket content.
+   * @returns Promise<object>
+   */
+  async fetchBasket() {
+    const response = await ajax.requestJson('./mock-data.json');
+    return response.body;
+  }
+
+
+  /**
+   * Get the basket data on load.
+   */
+   async firstUpdated() {
+    try {
+      this.basket = await this.fetchBasket();
+      console.log(this.basket);
+    } catch (error) {
+      // TODO: handle error
+      console.log(error);
+    }
+  }
+
   render() {
     return html`
       <p>test p x x</p>
