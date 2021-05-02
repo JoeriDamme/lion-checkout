59da8ec93ff4ffefdfc027278269d485791e9afe
Joeri Damme <joeri.damme@kpn.com>
2021-05-02 21:32:27 +0200

    feat: balance check.

--- components/checkout-payment-form.js
+++ components/checkout-payment-form.js
@@ -1,8 +1,21 @@
-import { LitElement, html } from '@lion/core';
+import { LitElement, html, css } from '@lion/core';
 import { ajax } from '@lion/ajax';
 import './checkout-select-bankaccount';
 
 export class CheckoutPaymentForm extends LitElement {
+
+  static get styles() {
+    return css`
+      :host .alert {
+        border: grey;
+        padding: 5px;
+      }
+
+      .alert-danger {
+        background-color: #fab1b1;
+      }
+    `
+  }
 
   /**
    * Get properties.
@@ -12,12 +25,42 @@ export class CheckoutPaymentForm extends LitElement {
       accounts: {
         type: Array,
       },
+      selectedAccount: {
+        type: Object,
+      },
+      totalPrice: {
+        type: Number,
+      }
     }
   }
 
   constructor() {
     super();
     this.accounts = [];
+
+    // listen to the changes on the bank account rich select.
+    this.addEventListener('changeBankAccount', (e) => {
+      const { detail } = e;
+      this.selectedAccount = this.accounts.find(account => account.accountId === detail.value.accountId);
+    });
+  }
+
+  /**
+   * When property update, check if selected account has been changed.
+   * If no balance, disable next button.
+   * @param {Map} changedProperties 
+   */
+    update(changedProperties) {
+    super.update(changedProperties);
+
+    // changedProperties is Map type.
+    if (changedProperties.has('selectedAccount')) {
+      const nameEvent = this.hasBalance() ? 'enableNextStep' : 'disableNextStep';
+
+      this.dispatchEvent(new CustomEvent(nameEvent, {
+        composed: true,
+      }));
+    }
   }
 
   /**
@@ -28,6 +71,11 @@ export class CheckoutPaymentForm extends LitElement {
     try {
       const result = await this.fetchBankAccounts();
       this.accounts = result.accounts;
+
+      // first item in array will be the default selected bank account.
+      this.selectedAccount = this.accounts[0];
+
+      this.hasBalance();
 
       // when entering the page, reserve the basket
       this.dispatchEvent(new CustomEvent('reserveBasket', {
@@ -48,6 +96,15 @@ export class CheckoutPaymentForm extends LitElement {
     return response.body;
   }
 
+  /**
+   * Is balance on selected bank account sufficient.
+   * @returns {Boolean}
+   */
+  hasBalance() {
+    console.log(this.selectedAccount);
+    return this.totalPrice < this.selectedAccount.balance;
+  }
+
   render() {
     return html`
       <link rel="stylesheet" href="../node_modules/flexboxgrid/css/flexboxgrid.css" type="text/css"> 
@@ -58,6 +115,11 @@ export class CheckoutPaymentForm extends LitElement {
             .options=${this.accounts} 
           >
           </checkout-select-bank-account>
+          <p><span>The balance for this account is: </span><span>${this.selectedAccount && this.selectedAccount.balance}</span></p>
+
+          ${this.selectedAccount && !this.hasBalance() ? html `
+            <div class="alert alert-danger">Unsufficient balance. Please select a different account</div>
+          ` : html``}
         </div>
       </div>
     `;
--- components/checkout-select-bankaccount.js
+++ components/checkout-select-bankaccount.js
@@ -20,9 +20,28 @@ export class CheckoutSelectBankAccount extends LitElement {
     this.options = [];
   }
 
+  handleChange() {
+    // @model-value-changed is called when element renders. Options are not set.
+    if (!this.options.length) {
+      return;
+    }
+
+    // get element
+    const element = this.shadowRoot.querySelector('lion-select-rich');
+
+    // dispatch event, so parent knows the selected item on change.
+    this.dispatchEvent(new CustomEvent('changeBankAccount', {
+      composed: true,
+      detail: {
+        index: element.checkedIndex,
+        value: this.options[element.checkedIndex],
+      }
+    }));
+  }
+
   render() {
     return html`
-    <lion-select-rich name="selectBankAccount" label="Select Bank Account">
+    <lion-select-rich name="selectBankAccount" label="Select Bank Account" @model-value-changed=${() => this.handleChange()}>
       ${this.options.map((option) => html`
         <lion-option .choiceValue=${option.accountId}>
           <strong>${option.name}</strong><br>${ibantools.friendlyFormatIBAN(option.iban)}
